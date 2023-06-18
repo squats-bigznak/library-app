10e5d51d1e209a94e978a3f3ff3690e2573dfcb4
squats-bigznak <88125284+squats-bigznak@users.noreply.github.com>
2023-06-17 22:57:15 -0700

    Code css layout

---
+++ library-app.js
@@ -0,0 +1,19 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function() {
+      console.log("'" + title + "'" + " " + "by" + " " + author + ", " + pages + " " + "pages," + " " + read)
+    }
+  }
+
+
+  function addBookToLibrary(title, author, pages, haveRead) {
+
+    const newBook = new Book(title, author, pages, haveRead);
+    myLibrary += newBook;
+    
+  }
