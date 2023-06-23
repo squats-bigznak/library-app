9268554cf784b8880e92e215a1ddf96b3079157e
squats-bigznak <88125284+squats-bigznak@users.noreply.github.com>
2023-06-23 11:43:25 -0700

    Debug hidden behavior

--- library-app.js
+++ library-app.js
@@ -6,10 +6,9 @@ function Book(title, author, pages, read, booknumber) {
     this.pages = pages
     if (read == false){
       this.read = "unread"
-      } else
-      {
+      } else {
       this.read = "read" 
-    }
+      }
     this.booknumber = booknumber
     this.info = function() {
       console.log("'" + title + "'" + " " + "by" + " " + author + ", " + pages + " " + "pages," + " " + read + " " + "Book Number" + " " + booknumber)
@@ -17,38 +16,38 @@ function Book(title, author, pages, read, booknumber) {
   }
 
   function addBookToLibrary(title, author, pages, haveRead) {
-    //we'll use bookNumber to store a data attribute 
-    //corresponding to the array position
     let bookNumber = myLibrary.length;
     const newBook = new Book(title, author, pages, haveRead, bookNumber);
-    myLibrary.push(newBook);  
-    
+    myLibrary.push(newBook);    
   }
 
 const bookTitle = document.querySelector("#title")
 const bookAuthor = document.querySelector("#author")
 const bookPages = document.querySelector("#pages")
 const bookRead = (document.querySelector('#readstatus'));
- 
 const addButton = document.querySelector(".addtolibrary")
 
 addButton.addEventListener('click', (event) => {
   addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.checked);
   updateLibrary();
+  // toggle();
+  // let element = document.querySelector(".form");
+  // let hidden = element.getAttribute("hidden");
+
+  // if (hidden) {
+  //    element.removeAttribute("hidden");
+  // } else {
+  //    element.setAttribute("hidden", "hidden");
+  // }
+  
 });
 
   addBookToLibrary('poo', 'pee', '123', false);
   addBookToLibrary('dung', 'weenjuice', '432', true);
   
-function removeBook(x){
-
-  myLibrary.splice(x, 1);
-  updateLibrary();
-}
 
 function updateLibrary(){
   const container = document.querySelector('.container');
-
   while(container.firstElementChild) {
     container.firstElementChild.remove();
   }
@@ -74,18 +73,63 @@ function updateLibrary(){
     pfield.textContent = myLibrary[i].pages;
     content.appendChild(pfield);
 
-    const rfield = document.createElement('readfield');
-    rfield.classList.add('readfield');
-    rfield.textContent = myLibrary[i].read;
-    content.appendChild(rfield);
+    // const rfield = document.createElement('readfield');
+    // rfield.classList.add('readfield');
+    // rfield.textContent = myLibrary[i].read;
+    // content.appendChild(rfield);
+    const button1 = document.createElement('button');
+    button1.innerText = myLibrary[i].read; ;
+    content.appendChild(button1);
 
-    const button = document.createElement('button');
-    button.innerText = 'Remove' ;
-    content.appendChild(button);
+    button1.addEventListener('click', () => {
+      toggleText(button1);
+      changeRead(i);
+    })
 
-    button.addEventListener('click', () => {
+    const button2 = document.createElement('button');
+    button2.innerText = 'remove' ;
+    content.appendChild(button2);
+
+    button2.addEventListener('click', () => {
       removeBook(i);
-      //remove element somehow//
     })
     }
+}
+
+function toggle(){
+  let element = document.querySelector(".form");
+  let hide = element.getAttribute("hidden");
+  if (hide) {
+     element.removeAttribute("hidden");
+     element.setAttribute("display", "flex");
+  } else {
+     element.setAttribute("hidden", "hidden");
+     element.removeAttribute("display", "flex");
+  }
+}
+
+function removeBook(x){
+  myLibrary.splice(x, 1);
+  updateLibrary();
+}
+
+function toggleText(button) {
+  if (button.innerHTML == "read") {
+      button.innerHTML = "unread";
+  } else if (button.innerHTML == "unread") {
+      button.innerHTML = "read";
+  } else {
+      null
+  }
+}
+
+function changeRead(x){
+  if (myLibrary[x].read == "read") {
+    myLibrary[x].read = "unread";
+} else if (myLibrary[x].read == "unread") {
+  myLibrary[x].read = "read";
+} else {
+    null
+}
+
 }
